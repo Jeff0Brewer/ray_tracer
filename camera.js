4354cb47c7878987aa5c35aabc1b733965e86332
Jeff <jeff@Jeffs-MacBook-Pro.local>
2021-02-20 17:57:20 -0700

    convert all vec operations to glMatrix

--- camera.js
+++ camera.js
@@ -19,9 +19,13 @@ class CameraController{
 
 	strafe(elapsed){
 		let speed = 10;
-		let d = add(mult_scalar(norm(sub(this.foc.slice(0, 2), this.pos.slice(0, 2))).concat([0]), this.strafe_sign.for*speed*elapsed/1000), mult_scalar(norm(cross3(sub(this.foc, this.pos), [0, 0, 1])), this.strafe_sign.lat*speed*elapsed/1000));
-		this.pos = add(this.pos, d);
-		this.foc = add(this.foc, d);
+		let d = vec3.add(
+			[0, 0, 0], 
+			vec3.scale([0, 0, 0], vec2.normalize([0, 0], vec2.subtract([0, 0], this.foc.slice(0, 2), this.pos.slice(0, 2))).concat([0]), this.strafe_sign.for*speed*elapsed/1000), 
+			vec3.scale([0, 0, 0], vec3.normalize([0, 0, 0], vec3.cross([0, 0, 0], vec3.subtract([0, 0, 0], this.foc, this.pos), [0, 0, 1])), this.strafe_sign.lat*speed*elapsed/1000)
+		);
+		vec3.add(this.pos, this.pos, d);
+		vec3.add(this.foc, this.foc, d);
 	}
 
 	add_strafe(s){
@@ -40,12 +44,12 @@ class CameraController{
 			let dx = this.r_spd * (e.clientX - this.mouse.x);
 			let dy = this.r_spd * (e.clientY - this.mouse.y);
 			
-			let dir = sub(this.foc, this.pos);
+			let dir = vec3.subtract([0, 0, 0], this.foc, this.pos);
 
 			mat4.rotate(this.rotation, mat4.create(), -dx, [0, 0, 1]);
-			mat4.rotate(this.rotation, this.rotation, -dy, norm(cross3(dir, this.up)));
+			mat4.rotate(this.rotation, this.rotation, -dy, vec3.normalize([0, 0, 0], vec3.cross([0, 0, 0], dir, this.up)));
 
-			this.foc = add(this.pos, vec3.transformMat4([0, 0, 0], dir, this.rotation));
+			vec3.add(this.foc, this.pos, vec3.transformMat4([0, 0, 0], dir, this.rotation));
 			vec3.transformMat4(this.up, this.up, this.rotation);
 
 			this.mouse.x = e.clientX;
@@ -58,8 +62,8 @@ class CameraController{
 	}
 
 	wheel(e){
-		let d = mult_scalar(sub(this.foc, this.pos), e.deltaY*this.z_spd);
-		this.pos = add(this.pos, d);
-		this.foc = add(this.foc, d);
+		let d = vec3.scale([0, 0, 0], vec3.subtract([0, 0, 0], this.foc, this.pos), e.deltaY*this.z_spd);
+		vec3.add(this.pos, this.pos, d);
+		vec3.add(this.foc, this.foc, d);
 	}
 }
--- geometry.js
+++ geometry.js
@@ -1,30 +1,30 @@
 class Plane{
 	constructor(position, normal, orientation, size){
 		this.c = position;
-		this.n = norm(normal);
+		this.n = vec3.normalize([0, 0, 0], normal);
 		this.s = size;
 
 		let axis = {
-			x: norm(orientation),
-			y: norm(cross3(normal, orientation)),
-			z: norm(normal)
+			x: vec3.normalize([0, 0, 0], orientation),
+			y: vec3.normalize([0, 0, 0], vec3.cross([0, 0, 0], normal, orientation)),
+			z: vec3.normalize([0, 0, 0], normal)
 		};
 		let sq = [].concat(
-			add(mult_scalar(axis.x, -size/2), mult_scalar(axis.y, -size/2)), [0, 0, 0],
-			add(mult_scalar(axis.x,  size/2), mult_scalar(axis.y, -size/2)), [0, 0, 0],
-			add(mult_scalar(axis.x,  size/2), mult_scalar(axis.y,  size/2)), [0, 0, 0],
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x, -size/2), vec3.scale([0, 0, 0], axis.y, -size/2)), [0, 0, 0],
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x,  size/2), vec3.scale([0, 0, 0], axis.y, -size/2)), [0, 0, 0],
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x,  size/2), vec3.scale([0, 0, 0], axis.y,  size/2)), [0, 0, 0],
 
-			add(mult_scalar(axis.x, -size/2), mult_scalar(axis.y, -size/2)), [0, 0, 0],
-			add(mult_scalar(axis.x,  size/2), mult_scalar(axis.y,  size/2)), [0, 0, 0],
-			add(mult_scalar(axis.x, -size/2), mult_scalar(axis.y,  size/2)), [0, 0, 0]
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x, -size/2), vec3.scale([0, 0, 0], axis.y, -size/2)), [0, 0, 0],
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x,  size/2), vec3.scale([0, 0, 0], axis.y,  size/2)), [0, 0, 0],
+			vec3.add([0, 0, 0], vec3.scale([0, 0, 0], axis.x, -size/2), vec3.scale([0, 0, 0], axis.y,  size/2)), [0, 0, 0]
 		);
 		let grid_size = 99;
 		let grid = [];
 		let sq_ind = 0;
 		for(let x_d = -size*grid_size/2 + size/2; x_d < size*grid_size/2; x_d += size){
-			let off_x = mult_scalar(axis.x, x_d);
+			let off_x = vec3.scale([0, 0, 0], axis.x, x_d);
 			for(let y_d = -size*grid_size/2 + size/2; y_d < size*grid_size/2; y_d += size, sq_ind++){
-				let offset = add(off_x, mult_scalar(axis.y, y_d)).concat((sq_ind % 2) == 0 ? [0, 0, 0] : [.4, .4, .4]);
+				let offset = vec3.add([0, 0, 0], off_x, vec3.scale([0, 0, 0], axis.y, y_d)).concat((sq_ind % 2) == 0 ? [0, 0, 0] : [.4, .4, .4]);
 				for(let i = 0; i < sq.length; i++){
 						grid.push(sq[i] + offset[i % offset.length]);
 				}
